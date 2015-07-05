d0d42435c45659e6a319e5edbb1c26f13b92c2c5
Grégoire <greg.autre@gmail.com>
2015-07-05 23:26:39 +0200

    changement de style

--- index.php
+++ index.php
@@ -9,10 +9,5 @@ require 'vendor/autoload.php';
 session_start(); // Le SDK nécessite l'utilisation de la session
 $connect = new FacebookConnect('1610526429223159', 'ab0fc4f736cc5ab4d7e8d5eb041b9d59');
 $user = $connect->connect('http://tchatrix.fr/index.php');
-if(is_string($user)){
-    echo "<a href='$user'>Se connecter avec Facebook</a>";
-}else{
-    echo "SELECT * FROM users WHERE facebook_id = {$user->getId()} OR email = {$user->getEmail()}";
-    var_dump($user->getName(),$user->getEmail(),$user->getLink());
-    echo '<a href="logout.php">Disconnect</a>';
-}
+
+include_once 'index.phtml';
